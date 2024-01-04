f301993a82145d67858c29016e8a5746438d8722
KaranGulve4342 <karangulve4342@gmail.com>
2024-01-04 16:16:43 +0530

    alan api key added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import alanBtn from '@alan-ai/alan-sdk-web';
+
 
 const App = () => {
+
+  useEffect(() => {
+    alanBtn({
+      key: process.env.alanKey,
+      onCommand: ({ command }) => {
+        if(command === 'testCommand'){
+          alert('This code was executed');
+        }
+      }
+    })
+  }, [])
+
   return (
       <div>
           <h1>Alan AI News Application</h1>
